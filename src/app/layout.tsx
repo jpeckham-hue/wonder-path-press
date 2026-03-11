72b12ce645e3ac54d7c03fed33eb2fc09bac7a5c
Jeff Peckham <jpeckham@gmail.com>
2026-03-11 15:41:04 +0700

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -45,6 +46,7 @@ export default function RootLayout({
         className={`${playfair.variable} ${inter.variable} antialiased font-sans flex min-h-screen flex-col`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
