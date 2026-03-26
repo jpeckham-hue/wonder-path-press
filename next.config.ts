9ae49e306c4b8311e106f180c0357e6edac7c029
Jeff Peckham <jpeckham@gmail.com>
2026-03-26 16:22:26 +0700

    Increase Next.js bodySizeLimit for base64 uploads

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    }
+  },
+  serverActions: {
+    bodySizeLimit: "10mb",
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
